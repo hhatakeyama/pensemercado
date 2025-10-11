a47d5399b8135e79c33d00bd8acb3147c5797bca
Heitor Hatakeyama <heitor_suh@hotmail.com>
2025-10-11 13:00:12 -0300

    Removido funções não utilizadas

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,4 @@
 import fetcher, { apiFetcher } from '@/utils/fetcher'
-import { polygonFetcher } from '@/utils/fetcher'
-import { fmpFetcher } from '@/utils/fetcher'
 import useSWR from 'swr'
 
 export default function useFetch(...args) {
@@ -25,32 +23,6 @@ export function useFetchApi(...args) {
   const newUrl = `/api${newQuery ? `${url}?${newQuery}` : url}`
 
   const { data, error, isValidating, mutate } = useSWR(url ? newUrl : null, { ...options, fetcher: apiFetcher })
-
-  return { data, error, isValidating, mutate }
-}
-
-export function useFetchPolygon(...args) {
-  const [params, options] = args
-  const [url, query = {}] = params
-
-  const queryObject = new URLSearchParams(query)
-  const newQuery = queryObject.toString()
-  const newUrl = `${newQuery ? `${url}?${newQuery}` : url}`
-
-  const { data, error, isValidating, mutate } = useSWR(url ? newUrl : null, { ...options, fetcher: polygonFetcher })
-
-  return { data, error, isValidating, mutate }
-}
-
-export function useFetchFmp(...args) {
-  const [params, options] = args
-  const [url, query = {}] = params
-
-  const queryObject = new URLSearchParams(query)
-  const newQuery = queryObject.toString()
-  const newUrl = `${newQuery ? `${url}?${newQuery}` : url}`
-
-  const { data, error, isValidating, mutate } = useSWR(url ? newUrl : null, { ...options, fetcher: fmpFetcher })
 
   return { data, error, isValidating, mutate }
 }
--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -1,49 +1,5 @@
 import api from "./api"
 
-export const brapiFetcher = async (url) => {
-  await fetch(url, {
-    headers: {
-      'Authorization': `Bearer ${process.env.NEXT_PUBLIC_BRAPI_API_TOKEN}`
-    }
-  })
-    .then((res) => res.json())
-}
-
-export const avFetcher = async (params) => {
-  // A SWR key será um objeto de parâmetros, por isso a desestruturação
-  const queryParams = new URLSearchParams({
-    ...params,
-    apikey: process.env.NEXT_PUBLIC_ALPHA_VANTAGE_API_TOKEN, // Adiciona a chave de API como parâmetro de URL
-  }).toString()
-
-  const url = `${process.env.NEXT_PUBLIC_ALPHA_VANTAGE_API_URL}?${queryParams}`
-
-  const res = await fetch(url)
-
-  if (!res.ok) {
-    const error = new Error('Erro ao buscar dados da Alpha Vantage.')
-    error.info = await res.json()
-    error.status = res.status
-    throw error
-  }
-
-  return res.json()
-}
-
-export const polygonFetcher = async (url) => {
-  return await fetch(`${process.env.NEXT_PUBLIC_POLYGON_IO_API_URL}${url}`, {
-    headers: {
-      'Authorization': `Bearer ${process.env.NEXT_PUBLIC_POLYGON_IO_API_TOKEN}`
-    }
-  })
-    .then((res) => res.json())
-}
-
-export const fmpFetcher = async (url) => {
-  return await fetch(`${process.env.NEXT_PUBLIC_FMP_API_URL}${url}&apikey=${process.env.NEXT_PUBLIC_FMP_API_TOKEN}`)
-    .then((res) => res.json())
-}
-
 export async function serverApiFetcher(url) {
   return fetch(`${process.env.NEXT_PUBLIC_ROOT_URL}${url}`).then(res => res.json())
 }
